1695e16c7df329c526a1c435694132339acc918d
Jacob <jfleish1@gmail.com>
2015-10-27 21:08:35 -0400

    test 3

--- public/partials/main.blade.php
+++ public/partials/main.blade.php
@@ -1,1 +1,61 @@
-[[test]]
+<script language="JavaScript">
+<!--
+function autoResize(id){
+    var newheight;
+    var newwidth;
+
+    if(document.getElementById){
+        newheight = document.getElementById(id).contentWindow.document .body.scrollHeight;
+        newwidth = document.getElementById(id).contentWindow.document .body.scrollWidth;
+    }
+
+    document.getElementById(id).height = (newheight) + "px";
+    document.getElementById(id).width = (newwidth) + "px";
+}
+//-->
+</script>
+
+<div class="row">
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+	<div class="col-md-4">
+		<iframe src="http://gifs.com/embed/yogQ7d" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
+	</div>
+</div>
